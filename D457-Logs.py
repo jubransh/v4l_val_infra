da009709f419ce61438e563d44fbe3dc829ec24e
faburakx <67681269+faburakx@users.noreply.github.com>
2022-01-18 16:48:29 +0200

    added Python script to collect csv files from hosts and combine them

---
+++ D457-Logs.py
@@ -0,0 +1,144 @@
+import logging
+import time
+import os
+import csv
+import paramiko
+
+paramiko.util.log_to_file("paramiko.log")
+
+time_stamp = time.strftime("%Y-%m-%d--%H-%M-%S")
+
+# hosts="143.185.115.150" # fadi's host
+hosts = "143.185.115.26" # shadi's host
+# hosts= "" # ashraf's host
+host_list=hosts.split(",")
+logs_folder="/home/nvidia/Logs/"
+
+target_folder = r"c:\log"
+download_folder=os.path.join(r"c:\D457-temp",time_stamp)
+
+
+def get_file_from_host(host,target_folder):
+    logger.info("connecting to :{}".format(host))
+    transport = paramiko.Transport((host,22))
+    transport.connect(None,"nvidia","nvidia")
+    sftp = paramiko.SFTPClient.from_transport(transport)
+    sids=sftp.listdir(logs_folder)
+    logger.info("going over SIDs")
+    for sid in sids:
+                logger.info("Found SID: "+sid+" in host: "+host)
+                tests = sftp.listdir(logs_folder + "/" + sid)
+                if not os.path.exists(os.path.join(target_folder,host)):
+                    try:
+                        os.makedirs(os.path.join(target_folder,host))
+                    except:
+                        pass
+                if "tests_results.csv" in sftp.listdir(logs_folder + "/" + sid + "/" ):
+                    logger.info("Found tests_results.csv in "+sid)
+                    if not os.path.exists(os.path.join(target_folder, host,sid)):
+                        try:
+                            os.makedirs(os.path.join(target_folder, host,sid))
+                        except:
+                            pass
+                    sftp.get(logs_folder + "/" + sid + "/tests_results.csv",os.path.join(target_folder,host,sid,"tests_results.csv"))
+                else:
+                    logger.error("tests_results.csv not found in SID: "+sid+ " in host: "+host)
+
+                for test in tests:
+                    if test != "tests_results.csv":
+                        if "iteration_summary.csv" in sftp.listdir(logs_folder + "/" + sid + "/" + test):
+                            if not os.path.exists(os.path.join(target_folder, host, sid,test)):
+                                try:
+                                    os.makedirs(os.path.join(target_folder, host, sid,test))
+                                except:
+                                    pass
+                            logger.info("getting iteration_summary.csv from Test:"+test+" from SID:"+sid+" from host:"+ host)
+                            sftp.get(logs_folder + "/" + sid + "/"+test+"/iteration_summary.csv",
+                                     os.path.join(target_folder, host, sid,test, "iteration_summary.csv"))
+                        else:
+                            logger.error("iteration_summary.csv not found in test: "+test+" SID: "+sid+ " in host: "+host)
+
+    if sftp: sftp.close()
+    if transport: transport.close()
+
+
+logger = logging.getLogger('fw_upgrade')
+logger.setLevel(logging.DEBUG)
+time_format = '%d/%m/%Y %H:%M:%S'
+formatter = logging.Formatter('%(threadName)s %(asctime)s.%(msecs)03d %(levelname)s [%(filename)s,'
+                              ' %(funcName)s(), Line %(lineno)d]: %(message)s', time_format)
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+if __name__ == '__main__':
+
+    if not os.path.exists(download_folder):
+        os.makedirs(download_folder)
+    if not os.path.exists(target_folder):
+        os.makedirs(target_folder)
+    logfile =os.path.join(target_folder,time_stamp + "_log.log")
+    fh = logging.FileHandler(logfile)
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    for host in host_list:
+        get_file_from_host(host,download_folder)
+    # download_folder = r"C:\Logs-D457"
+    # intialize "tests_results.csv" file
+    tests_results = open(os.path.join(target_folder,time_stamp+"_tests_results.csv"), "w+", newline='')
+    tests_results_writer = csv.writer(tests_results)
+    tests_header_added=False
+
+    # intialize "iteration_summary.csv" file
+    iteration_summary = open(os.path.join(target_folder,time_stamp+"_iteration_summary.csv"), "w+", newline='')
+    iteration_summary_writer = csv.writer(iteration_summary)
+    iterations_header_added=False
+
+    # go over hosts in logs folder
+    for host in os.listdir(download_folder):
+        logger.info("found host:"+host)
+        # go over SIDs in each host
+        for sid in os.listdir(os.path.join(download_folder,host)):
+            logger.info("Found SID: "+sid+" in host: "+host)
+            # check if "tests_results.csv" file exist, if exits copy data to tests_results.csv
+            if not os.path.exists(os.path.join(download_folder,host,sid,"tests_results.csv")):
+                logger.error("tests_results.csv file is missing in:"+ os.path.join(download_folder,host,sid))
+            else:
+                with open(os.path.join(download_folder,host,sid,"tests_results.csv")) as csv_file:
+                    csv_reader = csv.reader(csv_file, delimiter=',')
+                    logger.info("Adding data from:"+os.path.join(download_folder,host,sid,"tests_results.csv"))
+                    for index, row in enumerate(csv_reader):
+                        if index == 0:
+                            if not tests_header_added:
+                                tests_header_added=True
+                                tests_results_writer.writerow(row)
+                        else:
+                            tests_results_writer.writerow(row)
+                    tests_results.flush()
+            # go over tests in each SID
+            for test in os.listdir(os.path.join(download_folder,host,sid)):
+                logger.info("Found test: " + test +" in SID: "+sid + " in host: " + host)
+                if not os.path.exists(os.path.join(download_folder, host, sid,test, "iteration_summary.csv")):
+                    logger.error("iteration_summary.csv file is missing in:" + os.path.join(download_folder, host, sid,test))
+                else:
+                    with open(os.path.join(download_folder, host, sid,test, "iteration_summary.csv")) as csv_file:
+                        csv_reader = csv.reader(csv_file, delimiter=',')
+                        logger.info("Adding data from:"+os.path.join(download_folder, host, sid,test, "iteration_summary.csv"))
+                        for index, row in enumerate(csv_reader):
+                            if index == 0:
+                                if not iterations_header_added:
+                                    iterations_header_added=True
+                                    iteration_summary_writer.writerow(row)
+                            else:
+                                iteration_summary_writer.writerow(row)
+                        iteration_summary.flush()
+    tests_results.close()
+    iteration_summary.close()
+
+
+
+
+
