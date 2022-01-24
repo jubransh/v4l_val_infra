7c1774e249c9a4ddfccfcc9e124a277685b44b79
faburakx <fadix.abu.rakba@intel.com>
2022-01-24 08:21:26 +0200

    added metadata (in cam.init and in calmetrics) and removed delay (in all tests and in first frame delay and frames arrived metrics)

--- Stability.cpp
+++ Stability.cpp
@@ -205,20 +205,20 @@ public:
                 }
             }
             long startTime = TimeUtils::getCurrentTimestamp();
-            int slept=0;
+            //int slept=0;
             if (ColorUsed)
             {
                 color_collectFrames= true;
                 colorSensor.Start(AddFrame);
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                slept+=1;
+                //std::this_thread::sleep_for(std::chrono::seconds(1));
+                //slept+=1;
             }
             if (DepthUsed)
             {
                 depth_collectFrames = true;
                 depthSensor.Start(AddFrame);
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                slept+=1;
+               // std::this_thread::sleep_for(std::chrono::seconds(1));
+                //slept+=1;
             }
             if (IRUsed)
             {
@@ -238,14 +238,14 @@ public:
                 color_collectFrames= false;
                 colorSensor.Stop();
                 colorSensor.Close();
-                std::this_thread::sleep_for(std::chrono::seconds(1));
+                //std::this_thread::sleep_for(std::chrono::seconds(1));
             }
             if (DepthUsed)
             {
                 depth_collectFrames = false;
                 depthSensor.Stop();
                 depthSensor.Close();
-                std::this_thread::sleep_for(std::chrono::seconds(1));
+                //std::this_thread::sleep_for(std::chrono::seconds(1));
             }
             if (IRUsed)
             {
